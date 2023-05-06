e5446bcac15a9b2be8e08a67d8941112810ee4f3
ishcenko <107360109+ishcenko@users.noreply.github.com>
2023-05-06 13:38:06 +0300

    let intervalIdColor != null

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,7 +5,7 @@ const stopButton = document.querySelector('[data-stop]');
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 };
-let intervalIdColor = null;
+let intervalIdColor;
 
 
 startButton.addEventListener('clic', function () {
