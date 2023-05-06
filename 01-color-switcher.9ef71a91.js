bd3d34f7f253b1671e9b3a6f4ca435a52db434fa
ishcenko <107360109+ishcenko@users.noreply.github.com>
2023-05-06 10:38:32 +0000

    Deploying to gh-pages from @ ishcenko/goit-js-hw-09@e5446bcac15a9b2be8e08a67d8941112810ee4f3 🚀

--- 01-color-switcher.9ef71a91.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]");document.querySelector("[data-stop]");var e=null;t.addEventListener("clic",(function(){t.disabled=!0,e=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),t.addEventListener("click",(function(){t.disabled=!1,clearInterval(e)}))}();
-//# sourceMappingURL=01-color-switcher.9ef71a91.js.map
