d6b2fcadcdbfb7856dd059934f5cf971c7483809
ishcenko <107360109+ishcenko@users.noreply.github.com>
2023-05-06 12:32:54 +0300

    task color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const refs = {
+    startButton: document.querySelector('[data-start]'),
+    stopButton: document.querySelector('[data-stop]'),
+};
+
+let changeColorsOnClick = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+
+refs.startButton.addEventListener('clic', () => {
+    changeColorsOnClick();
+    changeColorsOnClick = setInterval(changeColorsOnClick, 1000);
+    refs.startButton.setAttribute('disabled', true);
+});
+refs.stopButton.addEventListener('clic', () => {
+    clearInterval(changeColorsOnClick);
+    refs.startButton.removeAttribute('disabled');
+});
+
+function changeColorsOnClick() {
+    document.body.style.backgroundColor = getRandomHexColor();
+};
 
