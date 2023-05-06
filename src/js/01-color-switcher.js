05903578db9af16bfb243283d461b9febfc9c023
ishcenko <107360109+ishcenko@users.noreply.github.com>
2023-05-06 13:36:20 +0300

    корегування коду

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,25 +1,21 @@
-const refs = {
-    startButton: document.querySelector('[data-start]'),
-    stopButton: document.querySelector('[data-stop]'),
-};
 
-let changeColorsOnClickStopStart = null;
-
-
-refs.startButton.addEventListener('clic', () => {
-    changeColorsOnClick();
-    changeColorsOnClickStopStart = setInterval(changeColorsOnClick, 1000);
-    refs.startButton.setAttribute('disabled', true);
-});
-refs.stopButton.addEventListener('clic', () => {
-    clearInterval(changeColorsOnClickStopStart);
-    refs.startButton.removeAttribute('disabled');
-});
-
-function changeColorsOnClick() {
-    document.body.style.backgroundColor = getRandomHexColor();
-};
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 };
+let intervalIdColor = null;
+
+
+startButton.addEventListener('clic', function () {
+    startButton.disabled = true;
+    intervalIdColor = setInterval(function () {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+});
+
+startButton.addEventListener('click', function () {
+    startButton.disabled = false;
+    clearInterval(intervalIdColor);
+});
